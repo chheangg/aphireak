18dd2159d72336a13c652a91e2189e8fda0c2c98
chheangg <balldremz@gmail.com>
2023-06-13 09:49:30 +0700

    Initialize backend server with Java Springboot

---
+++ server/src/main/java/com/chheang/aphireak/AphireakApplication.java
@@ -0,0 +1,13 @@
+package com.chheang.aphireak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AphireakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AphireakApplication.class, args);
+	}
+
+}
